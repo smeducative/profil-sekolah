6a317ab443b8b70fd8be7dc0a266db75ecd02825
akrindev <akrinmin@gmail.com>
2021-08-30 09:16:23 +0700

    init

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest()->paginate();
+
+        return view('pages.post.index', compact('posts'));
+    }
+
+    public function show(Request $request, Post $post)
+    {
+        return view('pages.post.show', compact('post'));
+    }
+
+    public function store()
+    {
+        return view('pages.post.create');
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'title' => ['required'],
+            'body'  => ['required']
+        ]);
+
+        Post::create([
+            'user_id'   => auth()->id(),
+            'title'     => $request->title,
+            'slug'      => Str::slug($request->title . '-' . Str::random(5)),
+            'cover'     => $request->cover,
+            'body'      => $request->body
+        ]);
+
+        session()->flash('success', 'Post baru telah di tambahkan');
+
+        return redirect()->route('post.index');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('pages.post.edit', compact('post'));
+    }
+
+    public function update(Post $post)
+    {
+        request()->validate([
+            'title' => ['required'],
+            'body'  => ['required']
+        ]);
+
+        $post->update(request()->all());
+
+        session()->flash('success', 'Post berhasil di update');
+
+        return back();
+    }
+
+    public function delete(Post $post)
+    {
+        $post->delete();
+
+        session()->flash('success', 'Post berhasil di hapus');
+
+        return back();
+    }
+}
---
+++ database/migrations/2021_08_30_013603_create_pengantars_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengantarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengantars', function (Blueprint $table) {
+            $table->id();
+            $table->longText('body')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengantars');
+    }
+}
